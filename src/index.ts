1e44cb924f5019cb4418cc67ef82f50a530cb8da
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-07-22 14:14:33 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+//ejercicio obligatorio - clase 1- Armar una página que contenga 2 noticias
+// Cada una debe tener:
+// Un título
+// Un subtítulo
+// Una imagen
+// Un enlace al portal donde está la noticia.
+// Los textos deben estar justificados y los títulos centrados,tener estilos y colores.
+// También aplicar una lista y un color de fondo.
+
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
